b826bd7ed5db6c880349a3e8b85d17b42e487977
Affee <affeemir29@gmail.com>
2024-02-05 13:16:03 +0530

    Calculator

---
+++ 050224ass1/index.js
@@ -0,0 +1,26 @@
+let num1 = prompt("Enter first number");
+let num2 = prompt("Enter second number");
+
+num1 = parseFloat(num1);
+num2 = parseFloat(num2);
+
+let sum = num1 + num2;
+alert("Sum is");
+alert(sum);
+
+let sub = num1 - num2;
+alert("Sub is");
+alert(sub);
+
+let mul = num1 * num2;
+alert("Multiply is");
+alert(mul);
+
+let div = num1 / num2;
+alert("Division is");
+alert(div);
+
+console.log(sum);
+console.log(sub);
+console.log(mul);
+console.log(div);
