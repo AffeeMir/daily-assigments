b613b53c6eddd77a98013ac4f9048f9e3d06b410
Affee <affeemir29@gmail.com>
2024-02-08 10:07:25 +0530

    Calculator

---
+++ 070224ass2/index2.js
@@ -0,0 +1,15 @@
+function sumValidNumbers(arr) {
+    let sum = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (typeof arr[i] === 'number' && !isNaN(arr[i])) {
+            sum += arr[i];
+        }
+    }
+
+    return sum;
+}
+
+const mixedArray = [1, 'two', 3, 'four', 5, 'six', true, null, undefined, 10];
+const result = sumValidNumbers(mixedArray);
+console.log("Sum of valid numbers:", result);
